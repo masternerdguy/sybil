1847654958c5885f67b1b04ff3a1179e180200a1
Nathan Wiehoff <masternerdguy@yahoo.com>
2025-02-06 18:33:06 -0500

    * :)

--- sybil_core/src/ssh_helper.erl
+++ sybil_core/src/ssh_helper.erl
@@ -15,14 +15,14 @@ new_tmux(Connection) ->
     ssh_helper:exec(Connection, "tmux new-session -s ollama -d").
 
 exec_in_tmux(Connection, Command) ->
-    ssh_helper:exec(Connection, "tmux send-keys -l '" ++ Command ++ "' Enter").
+    ssh_helper:exec(Connection, "tmux send-keys '" ++ replace_all(Command, "'", "") ++ "' Enter").
 
 capture_tmux(Connection) ->
     ssh_helper:exec(Connection, "tmux capture-pane -pS -").
 
 exec(Connection, Command) ->
     %% get channel
-    {ok, CR} = ssh_connection:session_channel(Connection, infinity),
+    {ok, CR} = ssh_connection:session_channel(Connection, 1000),
     
     %% send message
     ssh_connection:exec(Connection, CR, Command, 1000),
@@ -30,10 +30,20 @@ exec(Connection, Command) ->
     %% read result
     read([]).
 
+%% Internal API
+
 read(State) ->
     receive
         M -> 
             read(State ++ [M])
     after 1000 ->
         State
+    end.
+
+replace_all(String, Find, Replace) ->
+    case string:find(String, Find) of
+        nomatch -> String;
+        _ -> 
+            X = string:replace(String, Find, Replace),
+            replace_all(X, Find, Replace)
     end.
