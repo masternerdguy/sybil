dd53c85760a00d0aa34c687f3cdf063a56f78305
Nathan Wiehoff <masternerdguy@yahoo.com>
2025-02-06 15:34:34 -0500

    * working on ssh stuff

--- sybil_core/src/ssh_helper.erl
+++ sybil_core/src/ssh_helper.erl
@@ -1,8 +1,30 @@
 -module(ssh_helper).
 
--export([connect/0]).
+-export([connect/0, exec/2]).
 
 connect() ->
-    ssh:connect("172.20.128.2", 22,
+    %% open connection
+    {ok, CN} = ssh:connect("172.20.128.2", 22,
      [{user, "dubuntu"}, {password, "secret_password"},
-      {silently_accept_hosts, true}]).
+      {silently_accept_hosts, true}]),
+    
+    %% return connection
+    CN.
+
+exec(Connection, Command) ->
+    %% get channel
+    {ok, CR} = ssh_connection:session_channel(Connection, infinity),
+    
+    %% send message
+    ssh_connection:exec(Connection, CR, Command, 1000),
+
+    %% read result
+    read([]).
+
+read(State) ->
+    receive
+        M -> 
+            read(State ++ [M])
+    after 1000 ->
+        State
+    end.
