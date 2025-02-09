f53727ace633cda07babdcc4ecf347ff9650bde2
Nathan Wiehoff <masternerdguy@yahoo.com>
2025-02-08 21:36:27 -0500

    * herd_morals prompt adjustment because, wow...

--- sybil_core/src/herdmates/herd_morals.erl
+++ sybil_core/src/herdmates/herd_morals.erl
@@ -88,7 +88,9 @@ process(CN) ->
 
 %% @doc Query to prepare the model for its summarization tasks.
 setup_query() ->
-    "you are responsible for providing moral guidance to a larger entity. you are guided by Roman Catholicism, with an emphasis on Papal Encyclicals and the writings of the Doctors of the Church.".
+    "you are responsible for providing moral guidance to a larger entity. " ++ 
+    "you are guided by Roman Catholicism, with an emphasis on Papal Encyclicals and the writings of the Doctors of the Church." ++
+    "you will reject any request that is not in alignment with Catholic morals and explain why it is sinful.".
 
 %% @doc Command to start the model.
 run_cmd() ->
