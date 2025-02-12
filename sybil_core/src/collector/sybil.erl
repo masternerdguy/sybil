1388d5dc8a19afe9a15e76f886d49dbcc94ef6d4
Nathan Wiehoff <masternerdguy@yahoo.com>
2025-02-12 00:38:44 -0500

    * :)

--- sybil_core/src/collector/sybil.erl
+++ sybil_core/src/collector/sybil.erl
@@ -19,10 +19,11 @@ chat() ->
             FirstOutput = chat_take(Query),
 
             %% truncate the first output to 128 characters
-            Trunc = string:substr(FirstOutput, 1, 128),
+            TruncStart = string:substr(FirstOutput, 1, 128),
+            TruncEnd = reverse_substr(FirstOutput, 1, 128),
 
             %% pass two, including first truncated output
-            Output = chat_take(Query ++ io_lib:format(" | ~s | ", [Trunc])),
+            Output = chat_take(Query ++ io_lib:format(" | sybil's intermediate thoughts: ~s | ", [TruncStart ++ TruncEnd])),
 
             %% print results
             io:fwrite("~n# ~ts~n~n", [Output]),
@@ -47,3 +48,15 @@ chat_take(Query) ->
             %% return result
             Output
     end.
+
+%% @doc Helper function to get a substring from the end.
+reverse_substr(Str, Start, End) ->
+    %% reverse string
+    RS = lists:reverse(Str),
+
+    %% get substring
+    SS = string:substr(RS, Start, End),
+
+    %% reverse and return
+    XS = lists:reverse(SS),
+    XS.
