52836371863e3b24f28b86a8e0e24b3a0d08ba34
Nathan Wiehoff <masternerdguy@yahoo.com>
2025-02-12 00:39:14 -0500

    * write_log now only logs to a file

--- sybil_core/src/log_helper.erl
+++ sybil_core/src/log_helper.erl
@@ -9,9 +9,6 @@ write_log(Module, PID, Message) ->
     %% buld log string
     LS = io_lib:fwrite("~p | [~p] <~p> | ~s~n", [calendar:now_to_universal_time(os:timestamp()), Module, PID, Message]),
 
-    %% write to standard out
-    io:fwrite(LS),
-    
     %% write to log file
     file:write_file("sybil.log", LS, [append]).
 
