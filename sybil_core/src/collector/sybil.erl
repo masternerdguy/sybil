411caa6fdcf5b9edc5cb429ee99ffa9e5d3f1d1c
Nathan Wiehoff <masternerdguy@yahoo.com>
2025-02-12 00:40:35 -0500

    its erlang bro

--- sybil_core/src/collector/sybil.erl
+++ sybil_core/src/collector/sybil.erl
@@ -23,7 +23,7 @@ chat() ->
             TruncEnd = reverse_substr(FirstOutput, 1, 128),
 
             %% pass two, including first truncated output
-            Output = chat_take(Query ++ io_lib:format(" | some intermediate thoughts: ~s | ", [TruncEnd ++ TruncStart])),
+            Output = chat_take(Query ++ io_lib:format(" | some intermediate thoughts: ~s ~s | ", [TruncEnd, TruncStart])),
 
             %% print results
             io:fwrite("~n# ~ts~n~n", [Output]),
